b9c0a509e7c7a92225cabb465a6d0acf69fc95ae
Johnny Tisdale <johnnytisdale@gmail.com>
2021-09-02 20:19:54 -0500

    de-minified index.html, added react cdn to index.html, moved index.html from dist to root, added README.md, added code in src/index.js for testing, removed console.log in setOptions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import RandomCSS from "./js/RandomCss";
 
+//uncomment this code and open index.html in browser to test
+/*
+import ReactDOM  from "react-dom";
+const div = document.getElementById('app');
+if (div) { ReactDOM.render( <RandomCSS/>, div); }
+*/
+
 export default RandomCSS;
