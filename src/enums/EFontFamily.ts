a8f37087d01f4653a0feb54b7d4faeb50be658a2
Johnny Tisdale <johnnytisdale@gmail.com>
2023-07-02 19:20:09 -0700

    added font family enums

---
+++ src/enums/EFontFamily.ts
@@ -0,0 +1,19 @@
+enum EFontFamily {
+  ARIAL = 'Arial',
+  ARIAL_BLACK = 'Arial Black',
+  BOOKMAN = 'Bookman',
+  CANDARA = 'Candara',
+  COMIC_SANS_MS = 'Comic Sans MS',
+  COURIER = 'Courier',
+  COURIER_NEW = 'Courier New',
+  GARAMOND = 'Garamond',
+  GEORGIA = 'Georgia',
+  IMPACT = 'Impact',
+  PALATINO = 'Palatino',
+  ROBOTO = 'Roboto',
+  TIMES_NEW_ROMAN = 'Times New Roman',
+  TIMES = 'Times',
+  VERDANA = 'Verdana',
+}
+
+export default EFontFamily;
---
+++ src/enums/EFontFamilyGeneric.ts
@@ -0,0 +1,17 @@
+enum EFontFamilyGeneric {
+  CURSIVE = 'cursive',
+  EMOJI = 'emoji',
+  FANGSONG = 'fangsong',
+  FANTASY = 'fantasy',
+  MATH = 'math',
+  MONOSPACE = 'monospace',
+  SANS_SERIF = 'sans-serif',
+  SERIF = 'serif',
+  SYSTEM_UI = 'system-ui',
+  UI_MONOSPACE = 'ui-monospace',
+  UI_SANS_SERIF = 'ui-sans-serif',
+  UI_SERIF = 'ui-serif',
+  UI_ROUNDED = 'ui-rounded',
+};
+
+export default EFontFamilyGeneric;
