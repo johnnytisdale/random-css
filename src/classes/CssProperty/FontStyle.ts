5f4f1c50a5eac971f3dfc39f00aa0d708ca63b1c
Johnny Tisdale <johnnytisdale@gmail.com>
2023-07-02 21:17:46 -0700

    Removed unused vars from FontStyle

--- src/classes/CssProperty/FontStyle.ts
+++ src/classes/CssProperty/FontStyle.ts
@@ -6,8 +6,6 @@ export default class FontStyle extends CssProperty {
   protected acceptsLengths = false;
   protected acceptsKeywords = true;
   protected acceptsPercentages = false;
-  protected keywordGroups: Array<Array<string>>;
-  protected keywordLimit: number = 1;
   protected separator: string = ', ';
   public name: string = "fontStyle";
 
