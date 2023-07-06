fe16a6cd6e9b3aad940ab5b086758b6ed1658265
Johnny Tisdale <johnnytisdale@gmail.com>
2023-07-05 21:10:46 -0700

    added to glyph.test

--- tests/glyph.test.ts
+++ tests/glyph.test.ts
@@ -1,4 +1,7 @@
+import * as leetJSON from "../src/json/leet.json";
+import * as unicodeJSON from "../src/json/unicode.json";
 import Glyph from "../src/classes/Glyph";
+import Letter from "../src/enums/Letter";
 
 const glyph = new Glyph('a', false, false);
 
@@ -8,4 +11,29 @@ test("Glyph.name == 'glyph'", () => {
 
 test("Glyph.getRandomValue() == 'a'", () => {
   expect(glyph.getRandomValue()).toBe('a');
+});
+
+describe("unicode: String.fromCodePoint().length === 1", () => {
+  Object.values(Letter).forEach((letter: Letter) => {
+    unicodeJSON[letter].forEach(({ description, unicode }) => {
+      it(description, () => {
+        // TODO: Figure out why 'X, CANCELLATION' has a string length of 2!
+        expect(String.fromCodePoint(parseInt(unicode, 16)).length).toBe(
+          description === 'X, CANCELLATION'
+            ? 2
+            : 1
+        );
+      });
+    });
+  });
+});
+
+describe("leet: String.length > 0", () => {
+  Object.values(Letter).forEach((letter: Letter) => {
+    leetJSON[letter].forEach(leet => {
+      it(`${letter}: ${leet}`, () => {
+        expect(leet.length).toBeGreaterThan(0);
+      });
+    });
+  });
 });
