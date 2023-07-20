bd3ab57f0013df5e20d93d5f529e7bd6676a9f6a
Johnny Tisdale <johnnytisdale@gmail.com>
2023-07-20 01:09:46 -0700

    created FormSection component

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,11 +1,12 @@
 import "../styles/Form.scss";
 
 import ECssProperty from "../enums/CssProperty";
+import FormSection from "./FormSection";
+import Options from "../types/Options";
 import RandomCss from "./RandomCss";
 
 import * as React from "react";
 import { createRoot } from 'react-dom/client';
-import Options from "../types/Options";
 
 type Props = Record<string, never>;
 
@@ -111,171 +112,159 @@ export default class Form extends React.Component<Props, State> {
         <div id='dev-form'>
 
           {/* Global options */}
-          <div className='section'>
-            <div className='title'>global options</div>
-            <div className='options'>
-              <div className='option'>
-                <div className='label'>text</div>
-                <div className='input'>
-                  <input
-                    data-testid='randomcss-form-text'
-                    type='text'
-                    value={this.state.options.global.text}
-                    onChange={e => {
-                      const options = this.state.options;
-                      options.global.text = e.target.value;
-                      this.setState({ options: options });
-                    }}
-                  />
-                </div>
-              </div>
-              <div className='option'>
-                <div className='label'>unsafe</div>
-                <div className='input'>
-                  <input
-                    type='checkbox'
-                    checked={this.state.options.global.unsafe}
-                    onChange={e => {
-                      const options = this.state.options;
-                      options.global.unsafe = e.target.checked;
-                      this.setState({ options: options });
-                    }}
-                  />
-                </div>
-              </div>
-              <div className='option'>
-                <div className='label'>ignore spaces</div>
-                <div className='input'>
-                  <input
-                    type='checkbox'
-                    checked={this.state.options.global.ignoreSpaces}
-                    onChange={e => {
-                      const options = this.state.options;
-                      options.global.ignoreSpaces = e.target.checked;
-                      this.setState({ options: options });
-                    }}
-                  />
-                </div>
+          <FormSection title="global options">
+            <div className='option'>
+              <div className='label'>text</div>
+              <div className='input'>
+                <input
+                  data-testid='randomcss-form-text'
+                  type='text'
+                  value={this.state.options.global.text}
+                  onChange={e => {
+                    const options = this.state.options;
+                    options.global.text = e.target.value;
+                    this.setState({ options: options });
+                  }}
+                />
               </div>
             </div>
-          </div>
+            <div className='option'>
+              <div className='label'>unsafe</div>
+              <div className='input'>
+                <input
+                  type='checkbox'
+                  checked={this.state.options.global.unsafe}
+                  onChange={e => {
+                    const options = this.state.options;
+                    options.global.unsafe = e.target.checked;
+                    this.setState({ options: options });
+                  }}
+                />
+              </div>
+            </div>
+            <div className='option'>
+              <div className='label'>ignore spaces</div>
+              <div className='input'>
+                <input
+                  type='checkbox'
+                  checked={this.state.options.global.ignoreSpaces}
+                  onChange={e => {
+                    const options = this.state.options;
+                    options.global.ignoreSpaces = e.target.checked;
+                    this.setState({ options: options });
+                  }}
+                />
+              </div>
+            </div>
+          </FormSection>
 
           {/* css */}
-          <div className='section' id='css-options'>
-            <div className='title'>css options</div>
-            <div className='options'>
-              <div id='select-all-css' className='option'>
-                <div className='label'>select all</div>
-                <div className='input'>
-                  <input
-                    type='checkbox'
-                    checked={this.state.form.css.selectAll}
-                    onChange={e => this.toggleAll(e, true)}
-                  />
-                </div>
+          <FormSection id='css-options' title="css options">
+            <div id='select-all-css' className='option'>
+              <div className='label'>select all</div>
+              <div className='input'>
+                <input
+                  type='checkbox'
+                  checked={this.state.form.css.selectAll}
+                  onChange={e => this.toggleAll(e, true)}
+                />
               </div>
-              <div id='select-none-css' className='option'>
-                <div className='label'>select none</div>
-                <div className='input'>
-                  <input
-                    type='checkbox'
-                    checked={this.state.form.css.selectNone}
-                    onChange={e => this.toggleAll(e, false)}
-                  />
-                </div>
-              </div>
-              {
-                Object.values(ECssProperty).map((propertyName, index) => (
-                  <div key={index} className='option'>
-                    <div className='label'>{propertyName}</div>
-                    <div className='input'>
-                      <input
-                        type='checkbox'
-                        checked={this.state.options.css[propertyName]}
-                        onChange={e => {
-                          console.log(`toggling ${propertyName}`)
-                          this.toggleCssProperty(ECssProperty[propertyName], e.target.checked);
-                        }}
-                      />
-                    </div>
-                  </div>
-                ))
-              }
             </div>
-          </div>
+            <div id='select-none-css' className='option'>
+              <div className='label'>select none</div>
+              <div className='input'>
+                <input
+                  type='checkbox'
+                  checked={this.state.form.css.selectNone}
+                  onChange={e => this.toggleAll(e, false)}
+                />
+              </div>
+            </div>
+            {
+              Object.values(ECssProperty).map((propertyName, index) => (
+                <div key={index} className='option'>
+                  <div className='label'>{propertyName}</div>
+                  <div className='input'>
+                    <input
+                      type='checkbox'
+                      checked={this.state.options.css[propertyName]}
+                      onChange={e => {
+                        console.log(`toggling ${propertyName}`)
+                        this.toggleCssProperty(ECssProperty[propertyName], e.target.checked);
+                      }}
+                    />
+                  </div>
+                </div>
+              ))
+            }
+          </FormSection>
 
           { /* glyph */}
-          <div className='section' id='glyph-options'>
-            <div className='title'>glyph options</div>
-            <div className='options'>
-              <div id='1337' className='option'>
-                <div className='label'>1337</div>
-                <div className='input'>
-                  <input
-                    type='checkbox'
-                    checked={this.state.options.glyph.leet}
-                    onChange={e => {
-                      const options = this.state.options;
-                      options.glyph.leet = e.target.checked;
-                      this.setState({ options });
-                    }}
-                  />
-                </div>
-              </div>
-              <div id='unicode' className='option'>
-                <div className='label'>unicode</div>
-                <div className='input'>
-                  <input
-                    type='checkbox'
-                    checked={this.state.options.glyph.unicode}
-                    onChange={e => {
-                      const options = this.state.options;
-                      options.glyph.unicode = e.target.checked;
-                      this.setState({ options });
-                    }}
-                  />
-                </div>
+          <FormSection id='glyph-options' title="glyph options">
+            <div id='1337' className='option'>
+              <div className='label'>1337</div>
+              <div className='input'>
+                <input
+                  type='checkbox'
+                  checked={this.state.options.glyph.leet}
+                  onChange={e => {
+                    const options = this.state.options;
+                    options.glyph.leet = e.target.checked;
+                    this.setState({ options });
+                  }}
+                />
               </div>
             </div>
-          </div>
+            <div id='unicode' className='option'>
+              <div className='label'>unicode</div>
+              <div className='input'>
+                <input
+                  type='checkbox'
+                  checked={this.state.options.glyph.unicode}
+                  onChange={e => {
+                    const options = this.state.options;
+                    options.glyph.unicode = e.target.checked;
+                    this.setState({ options });
+                  }}
+                />
+              </div>
+            </div>
+          </FormSection>
 
           { /* export */}
-          <div className='section' id='export-options'>
-            <div className='title'>export</div>
-            <div className='options'>
-              <div id='export-textarea' className='option'>
-                <div className='input'>
-                  <textarea
-                    disabled={true}
-                    value={JSON.stringify(this.state.options)}
-                  />
-                </div>
-              </div>
-              <div id='export-button' className='option'>
-                <div className='input popup-container'>
-                  <button
-                    onClick={() => {
-                      navigator.clipboard.writeText(
-                        JSON.stringify(this.state.options)
-                      );
-                      this.setState(
-                        { copied: true },
-                        () => {
-                          setTimeout(
-                            () => this.setState({ copied: false }),
-                            1500
-                          );
-                        }
-                      );
-                    }}
-                  >
-                    copy
-                  </button>
-                  <div className={`popup-text${this.state.copied === true ? ' show' : this.state.copied === false ? ' hide' : ''}`}>copied!</div>
-                </div>
+          <FormSection id='export-options' title="export">
+            <div id='export-textarea' className='option'>
+              <div className='input'>
+                <textarea
+                  disabled={true}
+                  value={JSON.stringify(this.state.options)}
+                />
               </div>
             </div>
-          </div>
+            <div id='export-button' className='option'>
+              <div className='input popup-container'>
+                <button
+                  onClick={() => {
+                    navigator.clipboard.writeText(
+                      JSON.stringify(this.state.options)
+                    );
+                    this.setState(
+                      { copied: true },
+                      () => {
+                        setTimeout(
+                          () => this.setState({ copied: false }),
+                          1500
+                        );
+                      }
+                    );
+                  }}
+                >
+                  copy
+                </button>
+                <div className={`popup-text${this.state.copied === true ? ' show' : this.state.copied === false ? ' hide' : ''}`}>copied!</div>
+              </div>
+            </div>
+          </FormSection>
         </div>
       </>
     );
