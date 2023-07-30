cbb76927e34c20bd466850663547e1e2cc78d0cf
Johnny Tisdale <johnnytisdale@gmail.com>
2023-07-30 15:13:18 -0700

    CssProperty instead of ECssProperty

--- src/types/AppliedOptions.ts
+++ src/types/AppliedOptions.ts
@@ -1,8 +1,8 @@
-import ECssProperty from "../enums/CssProperty";
+import CssProperty from "../enums/CssProperty";
 import GlyphOption from "../enums/GlyphOption";
 
 interface AppliedOptions {
-  css: Array<ECssProperty>;
+  css: Array<CssProperty>;
   glyph: Array<GlyphOption>;
 }
 
