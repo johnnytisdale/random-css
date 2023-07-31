d0457ee90659aea13ddd86043a5a34e678a5555a
Johnny Tisdale <johnnytisdale@gmail.com>
2023-07-31 16:10:24 -0700

    changes to webpack config... returnless arrow function

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,40 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const webpack = require('webpack');
 
-module.exports = env => {
-
-	const isProduction = env.environment === 'production';
-
-	const plugins = [
+module.exports = env => ({
+	entry: './src/' + (
+		env.environment === 'production'
+			? 'js/index.js'
+			: 'components/Form.tsx'
+	),
+	module: {
+		rules: [
+			{
+				test: /\.(ts|js)x?$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					// Creates `style` nodes from JS strings
+					'style-loader',
+					// Translates CSS into CommonJS
+					'css-loader',
+					// Compiles Sass to CSS
+					'sass-loader',
+				]
+			}
+		]
+	},
+	output: {
+		filename: 'index.js',
+		path: path.resolve(__dirname, 'dist'),
+		library: 'random-css',
+		libraryTarget: 'umd',
+		umdNamedDefine: true
+	},
+	plugins: [
 		new webpack.DefinePlugin({
 			ENV: JSON.stringify(env.environment),
 		}),
@@ -15,57 +44,26 @@ module.exports = env => {
 			patterns: [
 				{ from: 'src/styles/random.css' },
 			]
-		})
-	];
-
-	if (!isProduction) {
-		plugins.push(
-			new HtmlWebpackPlugin({
-				template: 'src/html/index.html',
-				title: 'Random CSS'
-			})
-		);
-	}
-
-	return {
-		entry: `./src/${isProduction ? 'js/index.js' : 'components/Form.tsx'}`,
-		module: {
-			rules: [
-				{
-					test: /\.(ts|js)x?$/,
-					exclude: /node_modules/,
-					loader: 'babel-loader'
-				},
-				{
-					test: /\.s[ac]ss$/i,
-					use: [
-						// Creates `style` nodes from JS strings
-						'style-loader',
-						// Translates CSS into CommonJS
-						'css-loader',
-						// Compiles Sass to CSS
-						'sass-loader',
-					]
-				}
-			]
+		}),
+		...(
+			env.environment !== 'production' 
+				? [
+					new HtmlWebpackPlugin({
+						template: 'src/html/index.html',
+						title: 'Random CSS'
+					})
+				]
+				: []
+		)
+	],
+	resolve: {
+		extensions: ['.js', '.json', '.ts', '.tsx'],
+	},
+	...(env.environment !== 'production' && {
+		devtool: 'source-map',
+		mode: 'development',
+		optimization: {
+			minimize: false
 		},
-		output: {
-			filename: 'index.js',
-			path: path.resolve(__dirname, 'dist'),
-			library: 'random-css',
-			libraryTarget: 'umd',
-			umdNamedDefine: true
-		},
-		plugins: plugins,
-		resolve: {
-			extensions: ['.js', '.json', '.ts', '.tsx'],
-		},
-		...(!isProduction && {
-			devtool: 'source-map',
-			mode: 'development',
-			optimization: {
-				minimize: false
-			},
-		})
-	}
-};
+	})
+});
