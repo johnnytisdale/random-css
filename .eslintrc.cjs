82a68ce0b79531c4e4f4dd553ab5ae0a6e8c0f51
Johnny Tisdale <johnnytisdale@gmail.com>
2023-07-31 15:05:54 -0700

    in eslint config, overriding typescript no-var-requires rule for js files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,6 +4,14 @@ module.exports = {
   },
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   ignorePatterns: ['dist'],
+  overrides: [
+    {
+      files: ['*.js'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off'
+      }
+    }
+  ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   root: true,
