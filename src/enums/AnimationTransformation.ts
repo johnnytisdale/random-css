e8e52a7ed9f5a557b5a84bb867cd7890dcd8382e
Johnny Tisdale <johnnytisdale@gmail.com>
2023-08-01 16:21:55 -0700

    added enum AnimationTransformation

---
+++ src/enums/AnimationTransformation.ts
@@ -0,0 +1,10 @@
+enum AnimationTransformation {
+  ROTATE = 'rotate',
+  SCALE_DOWN = 'scaleDown',
+  SCALE_UP = 'scaleUp',
+  SKEW_X = 'skewX',
+  SKEW_XY = 'skewXY',
+  SKEW_Y = 'skewY'
+}
+
+export default AnimationTransformation;
