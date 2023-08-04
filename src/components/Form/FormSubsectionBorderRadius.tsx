b32857216a197952f90dcff89500abfc68f2a267
Johnny Tisdale <johnnytisdale@gmail.com>
2023-08-04 13:17:43 -0700

    added subsection to border radius

--- src/components/Form/FormSubsectionBorderRadius.tsx
+++ src/components/Form/FormSubsectionBorderRadius.tsx
@@ -4,6 +4,7 @@ import FormOptionRange from "./FormOptionRange";
 
 import * as React from "react";
 import FormOptionProbability from "./FormOptionProbability";
+import FormSubsection from "./FormSubsection";
 
 interface Props {
   option: BorderRadiusOptions;
@@ -21,44 +22,46 @@ export default function FormSubsectionBorderRadius ({
       label="borderRadius"
       setChecked={enabled => setOption({ enabled })}
     >
-      <FormOptionBoolean
-        checked={option?.slash === true}
-        {...{ disabled }}
-        label="slash"
-        setChecked={slash => setOption({ slash })}
-      />
-      <FormOptionProbability
-        label="slashProbability"
-        disabled={disabled || !option?.slash}
-        value={option?.slashProbability}
-        setProbability={slashProbability => setOption({ slashProbability })}
-      />
-      <FormOptionRange
-        {...{ disabled }}
-        max={DEFAULT_BORDER_RADIUS_MAX_CORNERS}
-        min={DEFAULT_BORDER_RADIUS_MIN_CORNERS}
-        maxLabel="maxCorners"
-        minLabel="minCorners"
-        maxValue={option.maxCorners}
-        minValue={option.minCorners}
-        setValues={(minCorners, maxCorners) => setOption({
-          maxCorners,
-          minCorners
-        })}
-      />
-      <FormOptionRange
-        {...{ disabled }}
-        max={DEFAULT_BORDER_RADIUS_MAX_RADIUS}
-        min={DEFAULT_BORDER_RADIUS_MIN_RADIUS}
-        maxLabel="maxRadius"
-        minLabel="minRadius"
-        maxValue={option.maxRadius}
-        minValue={option.minRadius}
-        setValues={(minRadius, maxRadius) => setOption({
-          maxRadius,
-          minRadius
-        })}
-      />
+      <FormSubsection>
+        <FormOptionBoolean
+          checked={option?.slash === true}
+          {...{ disabled }}
+          label="slash"
+          setChecked={slash => setOption({ slash })}
+        />
+        <FormOptionProbability
+          label="slashProbability"
+          disabled={disabled || !option?.slash}
+          value={option?.slashProbability}
+          setProbability={slashProbability => setOption({ slashProbability })}
+        />
+        <FormOptionRange
+          {...{ disabled }}
+          max={DEFAULT_BORDER_RADIUS_MAX_CORNERS}
+          min={DEFAULT_BORDER_RADIUS_MIN_CORNERS}
+          maxLabel="maxCorners"
+          minLabel="minCorners"
+          maxValue={option.maxCorners}
+          minValue={option.minCorners}
+          setValues={(minCorners, maxCorners) => setOption({
+            maxCorners,
+            minCorners
+          })}
+        />
+        <FormOptionRange
+          {...{ disabled }}
+          max={DEFAULT_BORDER_RADIUS_MAX_RADIUS}
+          min={DEFAULT_BORDER_RADIUS_MIN_RADIUS}
+          maxLabel="maxRadius"
+          minLabel="minRadius"
+          maxValue={option.maxRadius}
+          minValue={option.minRadius}
+          setValues={(minRadius, maxRadius) => setOption({
+            maxRadius,
+            minRadius
+          })}
+        />
+      </FormSubsection>
     </FormOptionBoolean>
   );
 }
