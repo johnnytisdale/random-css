3caeef32b48e9772ea84a1e68ec05a4869f057b6
Johnny Tisdale <johnnytisdale@gmail.com>
2023-08-04 15:24:01 -0700

    fixed circular reference

--- src/classes/CSS/CssProperty.ts
+++ src/classes/CSS/CssProperty.ts
@@ -1,32 +1,5 @@
-import CssOptions from "../../interfaces/CssOptions";
-import { DEFAULT_ANIMATION } from "./Animation";
-import { DEFAULT_BORDER_RADIUS } from "./BorderRadius";
-import { DEFAULT_BORDER_STYLE } from "./BorderStyle";
-import { DEFAULT_BORDER_WIDTH } from "./BorderWidth";
-import { DEFAULT_COLOR_OPTIONS } from "./ColorProperty";
-import { DEFAULT_FONT_FAMILY } from "./FontFamily";
-import { DEFAULT_FONT_STYLE } from "./FontStyle";
-import { DEFAULT_FONT_WEIGHT } from "./FontWeight";
 import { DEFAULT_GLOBAL_OPTIONS_UNSAFE } from "../../interfaces/GlobalOptions";
-import { DEFAULT_TEXT_DECORATION_LINE } from "./TextDecorationLine";
-import { DEFAULT_TEXT_DECORATION_STYLE } from "./TextDecorationStyle";
 import Randomizable from "../Randomizable";
-
-export const DEFAULT_CSS_OPTIONS: CssOptions = {
-  animation: { ...DEFAULT_ANIMATION },
-  backgroundColor: { ...DEFAULT_COLOR_OPTIONS },
-  borderColor: { ...DEFAULT_COLOR_OPTIONS },
-  borderRadius: { ...DEFAULT_BORDER_RADIUS },
-  borderStyle: { ...DEFAULT_BORDER_STYLE },
-  borderWidth: { ...DEFAULT_BORDER_WIDTH },
-  color: { ...DEFAULT_COLOR_OPTIONS },
-  fontFamily: { ...DEFAULT_FONT_FAMILY },
-  fontStyle: { ...DEFAULT_FONT_STYLE },
-  fontWeight: { ...DEFAULT_FONT_WEIGHT },
-  textDecorationColor: { ...DEFAULT_COLOR_OPTIONS },
-  textDecorationLine: { ...DEFAULT_TEXT_DECORATION_LINE },
-  textDecorationStyle: { ...DEFAULT_TEXT_DECORATION_STYLE }
-}
 
 export default abstract class CssProperty extends Randomizable {
 
--- src/interfaces/CssOptions.ts
+++ src/interfaces/CssOptions.ts
@@ -1,13 +1,15 @@
-import { AnimationOptions } from "../classes/CSS/Animation";
-import { BorderRadiusOptions } from "../classes/CSS/BorderRadius";
-import { BorderStyleOptions } from "../classes/CSS/BorderStyle";
+import { AnimationOptions, DEFAULT_ANIMATION } from "../classes/CSS/Animation";
+import { BorderRadiusOptions, DEFAULT_BORDER_RADIUS } from "../classes/CSS/BorderRadius";
+import { BorderStyleOptions, DEFAULT_BORDER_STYLE } from "../classes/CSS/BorderStyle";
 import ColorOptions from "./ColorOptions";
-import { FontFamilyOptions } from "../classes/CSS/FontFamily";
-import { FontStyleOptions } from "../classes/CSS/FontStyle";
-import { FontWeightOptions } from "../classes/CSS/FontWeight";
+import { DEFAULT_FONT_FAMILY, FontFamilyOptions } from "../classes/CSS/FontFamily";
+import { DEFAULT_FONT_STYLE, FontStyleOptions } from "../classes/CSS/FontStyle";
+import { DEFAULT_FONT_WEIGHT, FontWeightOptions } from "../classes/CSS/FontWeight";
 import { LengthOptions } from "../classes/CSS/LengthProperty";
-import { TextDecorationLineOptions } from "../classes/CSS/TextDecorationLine";
-import { TextDecorationStyleOptions } from "../classes/CSS/TextDecorationStyle";
+import { DEFAULT_TEXT_DECORATION_LINE, TextDecorationLineOptions } from "../classes/CSS/TextDecorationLine";
+import { DEFAULT_TEXT_DECORATION_STYLE, TextDecorationStyleOptions } from "../classes/CSS/TextDecorationStyle";
+import { DEFAULT_BORDER_WIDTH } from "../classes/CSS/BorderWidth";
+import { DEFAULT_COLOR_OPTIONS } from "../classes/CSS/ColorProperty";
 
 export default interface CssOptions {
     animation?: AnimationOptions;
@@ -24,3 +26,19 @@ export default interface CssOptions {
     textDecorationLine?: TextDecorationLineOptions;
     textDecorationStyle?: TextDecorationStyleOptions;
 }
+
+export const DEFAULT_CSS_OPTIONS: CssOptions = {
+    animation: { ...DEFAULT_ANIMATION },
+    backgroundColor: { ...DEFAULT_COLOR_OPTIONS },
+    borderColor: { ...DEFAULT_COLOR_OPTIONS },
+    borderRadius: { ...DEFAULT_BORDER_RADIUS },
+    borderStyle: { ...DEFAULT_BORDER_STYLE },
+    borderWidth: { ...DEFAULT_BORDER_WIDTH },
+    color: { ...DEFAULT_COLOR_OPTIONS },
+    fontFamily: { ...DEFAULT_FONT_FAMILY },
+    fontStyle: { ...DEFAULT_FONT_STYLE },
+    fontWeight: { ...DEFAULT_FONT_WEIGHT },
+    textDecorationColor: { ...DEFAULT_COLOR_OPTIONS },
+    textDecorationLine: { ...DEFAULT_TEXT_DECORATION_LINE },
+    textDecorationStyle: { ...DEFAULT_TEXT_DECORATION_STYLE }
+  }
