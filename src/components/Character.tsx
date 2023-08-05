f893d7b027bbf85a7752ff9b994fa6fa6d5dc036
Johnny Tisdale <johnnytisdale@gmail.com>
2023-08-05 11:51:36 -0700

    made Character.tsx a functional component

--- src/components/Character.tsx
+++ src/components/Character.tsx
@@ -4,6 +4,7 @@ import Randomizable from "../classes/Randomizable";
 
 import * as CSS from 'csstype';
 import * as React from "react";
+import { useCallback, useEffect, useMemo, useReducer, useRef } from "react";
 
 interface Props {
   character: string;
@@ -27,179 +28,192 @@ const DEFAULTS: Style = {
   borderWidth: '1px'
 }
 
-export default class Character extends React.Component<Props, State> {
+export default function Character({
+  character,
+  index,
+  randomizables,
+  reset,
+  size,
+  unsafe
+}: Props): React.ReactNode {
 
-  default: Style = {};
-  id: string;
-  interval: NodeJS.Timer | null = null;
-  ticks = 0;
+  const defaults = useRef<Style>({});
+  const id = useMemo(() => `character-${index}`, [index]);
+  const interval = useRef<NodeJS.Timer>(null);
+  const _randomizables = useRef<Randomizable[]>(randomizables);
 
-  constructor(props: Props) {
-    super(props);
-    this.id = `character-${this.props.index}`;
-    this.state = { glyph: this.props.character, style: {} };
-  }
+  const [state, setState] = useReducer(
+    (state: State, newState: Partial<State>) => ({ ...state, ...newState }),
+    { glyph: character, style: {} }
+  );
 
-  componentDidMount() {
+  const startTicking = useCallback(
+    () => {
+      interval.current = (
+        setInterval(() => {
+          let update = false;
+          const newState: State = {
+            glyph: state.glyph,
+            style: state.style,
+          };
+          for (const randomizable of _randomizables.current) {
+            if (!randomizable.isLimitReached()) {
+              continue;
+            }
+            const name = randomizable.name as CssProperty | 'glyph';
+            const newValue = randomizable.getRandomValue();
+            if (newValue === undefined) {
+              continue;
+            }
+            const comparator = name === 'glyph'
+              ? state.glyph
+              : state.style[name];
+            if (newValue !== comparator) {
+              if (name === 'glyph') {
+                newState.glyph = newValue;
+              } else {
+                newState.style[name] = newValue;
+              }
+              update = true;
+            }
+          }
+          if (update) {
+            setState(newState);
+          }
+        }, 300)
+      );
+    },
+    [_randomizables]
+  );
 
-    // Get the default CSS values.
-    const element = document.getElementById(this.id);
-    if (element === null) {
-      return;
-    }
-    const style = getComputedStyle(element);
-    Object.values(CssProperty).forEach((cssProperty: CssProperty) => {
+  // componentDidMount
+  useEffect(
+    () => {
+      // Get the default CSS values.
+      const element = document.getElementById(id);
+      if (element === null) {
+        return;
+      }
+      const style = getComputedStyle(element);
+      Object.values(CssProperty).forEach((cssProperty: CssProperty) => {
 
-      this.default[cssProperty] = Object.prototype.hasOwnProperty.call(
-        DEFAULTS,
-        cssProperty
-      )
-        ? DEFAULTS[cssProperty]
-        : style[cssProperty];
-    });
-    if (this.props.randomizables.length > 0) {
-      this.startTicking();
-    }
-  }
+        defaults.current[cssProperty] = Object.prototype.hasOwnProperty.call(
+          DEFAULTS,
+          cssProperty
+        )
+          ? DEFAULTS[cssProperty]
+          : style[cssProperty];
+      });
+      if (randomizables.length > 0) {
+        startTicking();
+      }
 
-  componentDidUpdate(): void {
-    if (this.props.randomizables.length === 0 && this.interval !== null) {
-      this.stopTicking();
-    } else if (this.props.randomizables.length > 0 && this.interval === null) {
-      this.startTicking();
-    }
-    const newState: State = {
-      glyph: this.state.glyph,
-      style: this.state.style
-    };
-    let update = false;
-    this.props.reset.css.forEach(property => {
-      update = true;
-      newState.style[property] = this.default[property];
+      // componentWillUnmount
+      return () => {
+        clearInterval(interval.current);
+        interval.current = null;
+      }
+    },
+    []
+  );
 
-    });
-    if (this.props.reset.glyph.length > 0) {
-      let hasGlyph = false;
-      /**
-       * TODO: Define randomizables as an object to reduce the complexity of
-       * checking for the existence of a particular randomizable.
-       */
-      for (const randomizable of this.props.randomizables) {
-        if (randomizable.name === 'glyph') {
-          hasGlyph = true;
-          break;
+  // componentDidUpdate
+  useEffect(
+    () => {
+      _randomizables.current = randomizables;
+      if (randomizables.length === 0 && interval.current !== null) {
+        clearInterval(interval.current);
+        interval.current = null;
+      } else if (randomizables.length > 0 && interval.current === null) {
+        startTicking();
+      }
+      const newState: State = {
+        glyph: state.glyph,
+        style: state.style
+      };
+      let update = false;
+      reset.css.forEach(property => {
+        update = true;
+        newState.style[property] = defaults.current[property];
+      });
+      if (reset.glyph.length > 0) {
+        let hasGlyph = false;
+        /**
+         * TODO: Define randomizables as an object to reduce the complexity of
+         * checking for the existence of a particular randomizable.
+         */
+        for (const randomizable of randomizables) {
+          if (randomizable.name === 'glyph') {
+            hasGlyph = true;
+            break;
+          }
+        }
+        /**
+         * Only reset the character to its default value if the other glyph option
+         * is not also checked.
+         */
+        if (hasGlyph === false) {
+          update = true;
+          newState.glyph = character;
         }
       }
-      /**
-       * Only reset the character to its default value if the other glyph option
-       * is not also checked.
-       */
-      if (hasGlyph === false) {
-        update = true;
-        newState.glyph = this.props.character;
+      if (update) {
+        setState(newState);
       }
-    }
-    if (update) {
-      this.setState(newState);
-    }
-  }
+    },
+    [interval.current, randomizables, reset, state]
+  );
 
-  private getClassName(): string {
-    return [
-      this.props.unsafe === false
-        ? `random-css-character-${String(this.props.size).replaceAll('.', '-')}`
+  const getClassName = useCallback(
+    () => [
+      unsafe === false
+        ? `random-css-character-${String(size).replaceAll('.', '-')}`
         : null,
-      ...Object.keys(this.state.style).map((cssProperty: CssProperty) => (
-        `random-css-${cssProperty}-${this.state.style[cssProperty]
+      ...Object.keys(state.style).map((cssProperty: CssProperty) => (
+        `random-css-${cssProperty}-${state.style[cssProperty]
           .replaceAll('"', '')
           .replaceAll(' ', '-')
           .replaceAll('%', '')
         }`
       ))
-    ].filter(Boolean).join(' ');
-  }
+    ].filter(Boolean).join(' '),
+    [size, state.style, unsafe]
+  );
 
   // TODO: Figure out why unchecking border color sets classname to
   // random-css-borderColor-rgb(0,-0,-0)
-  render(): React.ReactNode {
-    return (
-      <div
-        className={
-          this.props.unsafe ? 'random-css-character' : this.getClassName()
-        }
-        data-testid="character"
-        id={this.id}
-        style={
-          this.props.unsafe === false
-            ? null
-            : {
-              height: `${this.props.size * 1.1875}rem`,
-              width: `${this.props.size}rem`,
-              ...this.state.style,
-              ...(
-                this.state.style.fontWeight !== undefined && {
-                  fontWeight:
-                    this.state.style.fontWeight as CSS.Property.FontWeight
-                }
-              ),
-              ...(
-                this.state.style.textDecorationStyle !== undefined && {
-                  textDecorationStyle:
-                    this.state.style.textDecorationStyle as
-                      CSS.Property.TextDecorationStyle
-                }
-              ),
-            }
-          }
-      >
-        { /* TODO: figure out why this.state.glyph is sometimes undefined! */ }
-        {this.state.glyph ?? this.props.character}
-      </div>
-    );
-  }
-
-  startTicking(): void {
-    this.interval = setInterval(() => {
-      let update = false;
-      const newState: State = {
-        glyph: this.state.glyph,
-        style: this.state.style,
-      };
-      for (const randomizable of this.props.randomizables) {
-        if (!randomizable.isLimitReached()) {
-          continue;
-        }
-        const name = randomizable.name as CssProperty | 'glyph';
-        const newValue = randomizable.getRandomValue();
-        if (newValue === undefined) {
-          continue;
-        }
-        const comparator = name === 'glyph'
-          ? this.state.glyph
-          : this.state.style[name];
-        if (newValue !== comparator) {
-          if (name === 'glyph') {
-            newState.glyph = newValue;
-          } else {
-            newState.style[name] = newValue;
-          }
-          update = true;
-        }
+  return (
+    <div
+      className={
+        unsafe ? 'random-css-character' : getClassName()
       }
-      if (update) {
-        this.setState(newState);
-      }
-    }, 300);
-  }
-
-  stopTicking(): void {
-    if (this.interval != null) {
-      clearInterval(this.interval);
-      this.interval = null;
-    }
-  }
-
-  componentWillUnmount() {
-    this.stopTicking();
-  }
+      data-testid="character"
+      id={id}
+      style={
+        unsafe === false
+          ? null
+          : {
+            height: `${size * 1.1875}rem`,
+            width: `${size}rem`,
+            ...state.style,
+            ...(
+              state.style.fontWeight !== undefined && {
+                fontWeight:
+                  state.style.fontWeight as CSS.Property.FontWeight
+              }
+            ),
+            ...(
+              state.style.textDecorationStyle !== undefined && {
+                textDecorationStyle:
+                  state.style.textDecorationStyle as
+                    CSS.Property.TextDecorationStyle
+              }
+            ),
+          }
+        }
+    >
+      { /* TODO: figure out why state.glyph is sometimes undefined! */ }
+      {state.glyph ?? character}
+    </div>
+  );
 }
