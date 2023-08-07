ca0e0629de5a5bbf898b076745d8083e0ed85297
Johnny Tisdale <johnnytisdale@gmail.com>
2023-08-07 10:31:11 -0700

    deleted AppliedOptions as it's no longer used

--- src/interfaces/AppliedOptions.ts
+++
@@ -1,9 +0,0 @@
-import CssProperty from "../enums/CssProperty";
-import GlyphOption from "../enums/GlyphOption";
-
-interface AppliedOptions {
-  css: Array<CssProperty>;
-  glyph: Array<GlyphOption>;
-}
-
-export default AppliedOptions;
