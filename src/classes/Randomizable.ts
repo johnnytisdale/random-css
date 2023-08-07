c3eb7a45e0e2da5c36b4243181cbfcef5e30f651
Johnny Tisdale <johnnytisdale@gmail.com>
2023-08-06 23:53:15 -0700

    removed stuff from Randomizable that is no longer being used

--- src/classes/Randomizable.ts
+++ src/classes/Randomizable.ts
@@ -3,18 +3,6 @@ export default abstract class Randomizable {
     abstract getRandomValue(): string;
     abstract name: string;
 
-    private limit = this.getRandomNumber(1, 10);
-    private ticks = 0;
-
-    public isLimitReached(): boolean {
-        this.ticks++;
-        const isLimitReached = this.ticks >= this.limit;
-        if (isLimitReached) {
-            this.reset();
-        }
-        return isLimitReached;
-    }
-
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     protected getRandomArrayElement(array: Array<any>) {
         return array[this.getRandomNumber(0, array.length - 1)];
@@ -24,19 +12,10 @@ export default abstract class Randomizable {
         return this.getRandomNumber(0, 1) === 1;
     }
 
-    public getRandomNumber(
-        min: number,
-        max: number,
-        integerOnly = true
-    ) {
-        const rando = Math.random() * (max - min + 1) + min;
+    public getRandomNumber(min: number, max: number, integerOnly = true) {
+        const random = Math.random() * (max - min + 1) + min;
         return integerOnly === true
-            ? Math.floor(rando)
-            : parseFloat(rando.toFixed(2));
-    }
-
-    private reset() {
-        this.limit = this.getRandomNumber(1, 10);
-        this.ticks = 0;
+            ? Math.floor(random)
+            : parseFloat(random.toFixed(2));
     }
 }
