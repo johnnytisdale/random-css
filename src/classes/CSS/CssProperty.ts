0bd5c89eb4c6dab58b5eba6863327362f8c322cd
Johnny Tisdale <johnnytisdale@gmail.com>
2023-08-09 16:15:26 -0700

    typed name property of CssProperty class

--- src/classes/CSS/CssProperty.ts
+++ src/classes/CSS/CssProperty.ts
@@ -1,8 +1,10 @@
 import { DEFAULT_GLOBAL_OPTIONS_UNSAFE } from "../../interfaces/GlobalOptions";
+import ECssProperty from "../../enums/CssProperty";
 import Randomizable from "../Randomizable";
 
 export default abstract class CssProperty extends Randomizable {
   protected separator = " ";
+  public name: ECssProperty;
 
   constructor(protected unsafe = DEFAULT_GLOBAL_OPTIONS_UNSAFE) {
     super();
