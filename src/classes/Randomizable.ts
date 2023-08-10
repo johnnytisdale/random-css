7507a8a1ef0094bfbe58b448bd4a6433f4c19a39
Johnny Tisdale <johnnytisdale@gmail.com>
2023-08-09 22:22:36 -0700

    cleaned up Randomizable

--- src/classes/Randomizable.ts
+++ src/classes/Randomizable.ts
@@ -1,32 +1,7 @@
 import RandomizableName from "../types/RandomizableName";
 
 export default abstract class Randomizable {
-  abstract getRandomValue(): string;
-  abstract name: string;
-
-  public static array<T>(array: Array<T>) {
-    return array[Randomizable.number(0, array.length - 1)];
-  }
-
-  public static boolean(): boolean {
-    return Randomizable.number(0, 1) === 1;
-  }
-
-  public static decimal(min = 0, max = 1, places = 2): number {
-    if (min < 0) {
-      throw Error("getRandomDecimal: min cannot be less than 0.");
-    } else if (min > 1) {
-      throw Error("getRandomDecimal: max cannot be greater than 1.");
-    }
-    return parseFloat((Math.random() * (min - max) + max).toFixed(places));
-  }
-
-  public static number(min: number, max: number, integerOnly = true) {
-    const random = Math.random() * (max - min + 1) + min;
-    return integerOnly === true
-      ? Math.floor(random)
-      : parseFloat(random.toFixed(2));
-  }
+  public abstract name: RandomizableName;
 
   public static ignoreForSpaces: Record<RandomizableName, boolean> = {
     animation: false,
@@ -44,4 +19,25 @@ export default abstract class Randomizable {
     textDecorationLine: true,
     textDecorationStyle: true,
   };
+
+  public abstract getRandomValue(): string;
+
+  public static array<T>(array: Array<T>) {
+    return array[Randomizable.number(0, array.length - 1)];
+  }
+
+  public static boolean(): boolean {
+    return Randomizable.number(0, 1) === 1;
+  }
+
+  public static decimal(min = 0, max = 1, places = 2): number {
+    return parseFloat((Math.random() * (min - max) + max).toFixed(places));
+  }
+
+  public static number(min: number, max: number, integer = true) {
+    const random = Math.random() * (max - min + 1) + min;
+    return integer === true
+      ? Math.floor(random)
+      : parseFloat(random.toFixed(2));
+  }
 }
