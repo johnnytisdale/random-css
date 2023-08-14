95ea63412a0b2e65cbe8c0ad808c2773bd8a6c95
Johnny Tisdale <johnnytisdale@gmail.com>
2023-08-13 21:02:34 -0700

    supporting rgba (added alpha)

--- src/classes/CSS/ColorProperty.ts
+++ src/classes/CSS/ColorProperty.ts
@@ -1,5 +1,8 @@
 import ColorKeyword from "../../enums/ColorKeyword";
 import ColorOptions, {
+  DEFAULT_COLOR_ALPHA,
+  DEFAULT_COLOR_ALPHA_MAX,
+  DEFAULT_COLOR_ALPHA_MIN,
   DEFAULT_COLOR_KEYWORDS,
   DEFAULT_COLOR_MAX,
   DEFAULT_COLOR_MIN,
@@ -8,6 +11,9 @@ import CssProperty from "./CssProperty";
 import Randomizable from "../Randomizable";
 
 export default abstract class ColorProperty extends CssProperty {
+  private alpha: boolean;
+  private aMax: number;
+  private aMin: number;
   private bMax: number;
   private bMin: number;
   private gMax: number;
@@ -21,6 +27,9 @@ export default abstract class ColorProperty extends CssProperty {
     protected external: boolean
   ) {
     super(external);
+    this.alpha = options.alpha ?? DEFAULT_COLOR_ALPHA;
+    this.aMax = options.aMax ?? DEFAULT_COLOR_ALPHA_MAX;
+    this.aMin = options.aMin ?? DEFAULT_COLOR_ALPHA_MIN;
     this.bMax = options.bMax ?? DEFAULT_COLOR_MAX;
     this.bMin = options.bMin ?? DEFAULT_COLOR_MIN;
     this.gMax = options.gMax ?? DEFAULT_COLOR_MAX;
@@ -35,7 +44,10 @@ export default abstract class ColorProperty extends CssProperty {
       const red = Randomizable.number(this.rMin, this.rMax);
       const green = Randomizable.number(this.gMin, this.gMax);
       const blue = Randomizable.number(this.bMin, this.bMax);
-      return `rgb(${red}, ${green}, ${blue})`;
+      const alpha = this.alpha
+        ? ", " + String(Randomizable.decimal(this.aMin, this.aMax))
+        : "";
+      return `rgb(${red}, ${green}, ${blue}${alpha})`;
     }
     return Randomizable.array(this.colorKeywords);
   }
