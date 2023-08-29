9b5b7e5fb8e5f1c9fc95893ae2c1489f7295dd23
Johnny Tisdale <johnnytisdale@gmail.com>
2023-08-28 17:09:43 -0700

    renamed slash/slashProbability to elliptical/ellipticalProbability

--- src/interfaces/BorderRadiusConfig.ts
+++ src/interfaces/BorderRadiusConfig.ts
@@ -1,10 +1,10 @@
 import Config from "./Config";
 
 export default interface BorderRadiusConfig extends Config {
+  elliptical?: boolean;
+  ellipticalProbability?: number;
   maxCorners?: number;
   minCorners?: number;
   maxRadius?: number;
   minRadius?: number;
-  slash?: boolean;
-  slashProbability?: number;
 }
