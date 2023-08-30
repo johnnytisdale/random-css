f85d8c6bb2be835d07e727540f8d493231f95819
Johnny Tisdale <johnnytisdale@gmail.com>
2023-08-30 12:50:22 -0700

    created enums for leet glyphs

---
+++ src/enums/LeetGlyph.ts
@@ -0,0 +1,224 @@
+export enum LeetGlyphA {
+  AMPERSAND = "@",
+  FOUR = "4",
+  SLASHES = "/\\",
+  SLASHES_HYPHEN = "/-\\",
+  SLASHES_UNDERSCORE = "/_\\",
+}
+
+export enum LeetGlyphB {
+  EIGHT = "8",
+  J_THREE = "j3",
+  L_O = "lo",
+  ONE_EIGHT = "18",
+  ONE_THREE = "13",
+  PIPE_COLON = "|:",
+  PIPE_CURCLY_BRACKET = "|}",
+  PIPE_EIGHT = "|8",
+  PIPE_O = "|o",
+  PIPE_THREE = "|3",
+  SIX = "6",
+}
+
+export enum LeetGlyphC {
+  CENT = "¢,",
+  COPYRIGHT = "©",
+  CURLY_BRACKET = "{",
+  LESS_THAN = "\u003c",
+  PARENTHESIS = "(",
+  SQUARE_BRACKET = "[",
+}
+
+export enum LeetGlyphD {
+  COLON_PIPE_PARENTHESIS = ":|)",
+  GREATER_THAN = "|\u003e",
+  PIPE_CURLY_BRACKET = "|}",
+  PIPE_SQUARE_BRACKET = "|]",
+}
+
+export enum LeetGlyphE {
+  POUND = "£",
+  THREE = "3",
+}
+
+export enum LeetGlyphF {
+  P_H = "ph",
+  PIPE_EQUALS = "|=",
+  PIPE_HASH = "|#",
+  PIPE_SLASH = '|"',
+}
+
+export enum LeetGlyphG {
+  C_MINUS = "C-",
+  HYPHEN = "-",
+  SIX = "6",
+  SQUARE_BRACKET = "[",
+  SQUARE_BRACKET_PLUS = "[+",
+}
+
+export enum LeetGlyphH {
+  COLONS_HYPHEN = ":-:",
+  CURLY_BRACKETS_EQUALS = "{=}",
+  CURLY_BRACKETS_HYPHEN_INNER = "{-}",
+  CURLY_BRACKETS_HYPHEN_OUTER = "}-{",
+  CURLY_BRACKETS = "}{",
+  FOUR = "4",
+  I_PLUS_I = "I+I",
+  PARENTHESES_HYPHEN_INNER = "(-)",
+  PARENTHESES_HYPHEN_OUTER = ")-(",
+  PIPE_HYPHEN_PIPE = "|-|",
+  PIPES_EQUALS = "|=|",
+  SLASHES_HYPHEN = "/-/",
+  SQUARE_BRACKETS_HYPHEN = "[-]",
+  SQUARE_BRACKETS_EQUALS = "[=]",
+}
+
+export enum LeetGlyphI {
+  EXCLAMATION = "!",
+  NINE = "9",
+  ONE = "1",
+  PIPE = "|",
+}
+
+export enum LeetGlyphJ {
+  UNDERSCORE_CURLY_BRACKET = "_}",
+  UNDERSCORE_PARENTHESIS = "_)",
+  UNDERSCORE_PIPE = "_|",
+  UNDERSCORE_SEVEN = "_7",
+  UNDERSCORE_SLASH = "_/",
+  UNDERSCORE_SQUARE_BRACKET = "_]",
+}
+
+export enum LeetGlyphK {
+  L_CURLY_BRACKET = "l{",
+  L_LESS_THAN = "l\u003c",
+  ONE_LESS_THAN = "1\u003c",
+  PIPE_CURLY_BRACKET = "|{",
+  PIPE_LESS_THAN = "|\u003c",
+}
+
+export enum LeetGlyphL {
+  PIPE_UNDERSCORE = "|_",
+  PIPE = "|",
+  ONE = "1",
+  SQUARE_BRACKETS = "][",
+}
+
+export enum LeetGlyphM {
+  CARATS = "^^",
+  N_SLASH = "N\\",
+  PARENTHESES_V = "(V)",
+  PIPES_SLASHES = "|\\/|",
+  SLASHES = "/\\/\\",
+  SLASHES_X = "/X\\",
+}
+
+export enum LeetGlyphN {
+  PIPES_SLASH = "|\\|",
+  SLASH_V = "/V",
+  SLASHES = "/\\/",
+}
+
+export enum LeetGlyphO {
+  CURLY_BRACKETS = "{}",
+  LESS_THAN_GREATER_THAN = "\u003c\u003e",
+  PARENTHESES = "()",
+  SQUARE_BRACKETS = "[]",
+  ZERO = "0",
+}
+
+export enum LeetGlyphP {
+  GREATER_THAN = "|\u003e",
+  PIPE_A_DEGREES = "|Â°",
+  PIPE_ASTERISK = "|*",
+  PIPE_D = "|D",
+  PIPE_LOWERCASE_O = "|o",
+  PIPE_SEVEN = "|7",
+  PIPE_UPPERCASE_O = "|O",
+  SLASH_O = "/o",
+  SQUARE_BRACKETS_D = "[]D",
+}
+
+export enum LeetGlyphQ {
+  K_W = "kw",
+  NINE = "9",
+  O_UNDERSCORE = "O_",
+  PARENTHESES_COMMA = "(,)",
+  ZERO_COMMA = "0,",
+}
+
+export enum LeetGlyphR {
+  ONE_TWO = "12",
+  PIPE_2 = "|2",
+  PIPE_CARAT = "|^",
+  REGISTERED = "®",
+}
+
+export enum LeetGlyphS {
+  DOLLAR = "$",
+  FIVE = "5",
+  SECTION = "§",
+}
+
+export enum LeetGlyphT {
+  APOSTROPHES_PIPE = "\u0027|\u0027",
+  APOSTROPHES_SQUARE_BRACKETS = "\u0027][\u0027",
+  HYPHENS_PIPE = "-|-",
+  PLUS = "+",
+  SEVEN = "7",
+  SEVEN_TICK = "7`",
+  TICKS_PIPE = "`|`",
+  TILDES_PIPE = "~|~",
+}
+
+export enum LeetGlyphU {
+  BACK_SLASHES_UNDERSCORE = "\\_\\",
+  CURLY_BRACKETS_UNDERSCORE = "{_}",
+  FORWARD_SLASHES_UNDERSCORE = "/_/",
+  PARENTHESES_UNDERSCORE = "(_)",
+  PIPES_UNDERSCORE = "|_|",
+  SLASHES_UNDERSCORE = "\\_/",
+  SQUARE_BRACKETS_UNDERSCORE = "[_]",
+}
+
+export enum LeetGlyphV {
+  SLASHES = "\\/",
+}
+
+export enum LeetGlyphW {
+  APOSTROPHE_SLASHES = "\u0027//",
+  D_TREMA = "Ð¨",
+  DOUBLE_SLASHES = "\\\\//\\\\//",
+  PARENTHESES_SLASHES = "(/\\)",
+  PIPES_SLASHES = "|/\\|",
+  SLASHES = "\\/\\/",
+  SLASHES_APOPSTROPHE = "\\\\\u0027",
+  SLASHES_CARAT = "\\^/",
+  SLASHES_UNDERSCORES_PIPE = "\\_|_/",
+  SLASHES_V = "\\V/",
+  SLASHES_X = "\\X/",
+  TWO_U = "2u",
+  V_V = "VV",
+}
+
+export enum LeetGlyphX {
+  ASTERISK = "*",
+  CURLY_BRACKETS = "}{",
+  GREATER_THAN_LESS_THAN = "\u003e\u003c",
+  PARENTHESES = ")(",
+  PERCENT = "%",
+}
+
+export enum LeetGlyphY {
+  SLASHES_PIPE = "\\|/",
+  TICK_SLASH = "`/",
+  YEN = "¥",
+}
+
+export enum LeetGlyphZ {
+  FIVE = "5",
+  GREATER_THAN_UNDERSCORE_COMMA = "\u003e_,",
+  PARENTHESES_SLASH = "(/)",
+  SEVEN_UNDERSCORE = "7_",
+  TWO = "2",
+}
